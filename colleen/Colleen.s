ccebe1c65eec8fa5b614371ce6d4d7983898130c
SJuery <gnightelemental@gmail.com>
2018-09-13 15:42:33 -0700

    Nearly done with Grace ASM (Just need to give it macros [Thats one of the restrictions]). Changed Makefiles to let them delete files created by our program

--- colleen/Colleen.s
+++ colleen/Colleen.s
@@ -1,5 +1,5 @@
 .data
-.hello: .string ".data%1$c.hello: .string %3$c%4$s%3$c%1$c%1$c.text%1$c.globl _main%1$c#function to print whole string%1$c_printstring:%1$c%2$cpush %%rbp%1$c%2$cmov %%rsp, %%rbp%1$c%2$cmov $10, %%esi%1$c%2$cmov $9, %%edx%1$c%2$cmov $34, %%ecx%1$c%2$clea .hello(%%rip), %%r8%1$c%2$clea .hello(%%rip), %%rdi%1$c%2$ccall _printf%1$c%2$cleave%1$c%2$cret%1$c%1$c_main:%1$c%2$cpush %%rbp%1$c%2$cmov %%rsp, %%rbp%1$c%2$ccall _printstring #Calls function to print whole string%1$c%2$cleave%1$c%2$cret%1$c"
+quine: .string ".data%1$cquine: .string %3$c%4$s%3$c%1$c%1$c.text%1$c.globl _main%1$c#function to print whole string%1$c_printstring:%1$c%2$cpush %%rbp%1$c%2$cmov %%rsp, %%rbp%1$c%2$cmov $10, %%esi%1$c%2$cmov $9, %%edx%1$c%2$cmov $34, %%ecx%1$c%2$clea quine(%%rip), %%r8%1$c%2$clea quine(%%rip), %%rdi%1$c%2$ccall _printf%1$c%2$cleave%1$c%2$cret%1$c%1$c_main:%1$c%2$cpush %%rbp%1$c%2$cmov %%rsp, %%rbp%1$c%2$ccall _printstring #Calls function to print whole string%1$c%2$cleave%1$c%2$cret%1$c"
 
 .text
 .globl _main
@@ -10,8 +10,8 @@ _printstring:
 	mov $10, %esi
 	mov $9, %edx
 	mov $34, %ecx
-	lea .hello(%rip), %r8
-	lea .hello(%rip), %rdi
+	lea quine(%rip), %r8
+	lea quine(%rip), %rdi
 	call _printf
 	leave
 	ret
